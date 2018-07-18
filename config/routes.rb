029e51aea8111c206928ebc3162982c1295ea2fa
S-H-GAMELINKS <gamelinks007@gmail.com>
2018-07-18 11:47:21 +0900

    rails g devise user

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  devise_for :users
   root 'maps#index'
   resources :maps
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
