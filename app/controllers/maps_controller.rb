137711bb49fd148a7281396ac6437f987d3c515f
S-H-GAMELINKS <gamelinks007@gmail.com>
2018-07-18 17:31:40 +0900

    fix

--- app/controllers/maps_controller.rb
+++ app/controllers/maps_controller.rb
@@ -42,7 +42,7 @@ class MapsController < ApplicationController
             file.write @s3.get_object(:bucket => ENV['S3_BUCKET_NAME'] , :key => @map.image.path.to_s).body.read
         end
 
-        @exif = EXIFR::JPEG.new("./public/temp.jpg")
+        @exif = EXIFR::JPEG.new("public/temp.jpg")
         @map.update(:longitude => @exif.gps.class == nil.class ? nil : @exif.gps.longitude, 
                     :latitude => @exif.gps.class == nil.class ? nil : @exif.gps.latitude, 
                     :date => @exif.date_time_original.class == nil.class ? nil : @exif.date_time_original)
@@ -66,7 +66,7 @@ class MapsController < ApplicationController
           file.write client.get_object(:bucket => ENV['S3_BUCKET_NAME'] , :key => @map.image.path.to_s).body.read
         end
 
-        @exif = EXIFR::JPEG.new("./public/temp.jpg")
+        @exif = EXIFR::JPEG.new("public/temp.jpg")
         @map.update(:longitude => @exif.gps.class == nil.class ? nil : @exif.gps.longitude, 
                     :latitude => @exif.gps.class == nil.class ? nil : @exif.gps.latitude, 
                     :date => @exif.date_time_original.class == nil.class ? nil : @exif.date_time_original)
