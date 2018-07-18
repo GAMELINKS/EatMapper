c5b8190d055e06ca796121b087296ef1f28732c5
S-H-GAMELINKS <gamelinks007@gmail.com>
2018-07-18 17:32:32 +0900

    fix

--- app/controllers/maps_controller.rb
+++ app/controllers/maps_controller.rb
@@ -62,7 +62,7 @@ class MapsController < ApplicationController
     respond_to do |format|
       if @map.update(map_params)
 
-        File.open("./public/temp.jpg","wb") do |file|
+        File.open("public/temp.jpg","wb") do |file|
           file.write client.get_object(:bucket => ENV['S3_BUCKET_NAME'] , :key => @map.image.path.to_s).body.read
         end
 
